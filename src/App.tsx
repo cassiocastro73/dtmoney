cb746f997d3d157164e355d4f8779ed6078971d2
Cássio Castro <cassiocastro222@gmail.com>
2022-03-14 13:08:32 -0300

    Criação e estilização inicial do Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from './styles/global'
 import { Header } from './components/Header/index'
+import Dashboard from './components/Dashboard';
 
 export function App() {
   return (
     <>
       <Header></Header>
+      <Dashboard />
      <GlobalStyle />
     </>
   );
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import IncomeImg from '../../assets/income.svg'
+import OutcomeImg from '../../assets/outcome.svg'
+import TotalImg from '../../assets/total.svg'
+import { Container } from './styles';
+
+export function Summary() {
+  return (
+    <Container>
+        <div>
+            <header>
+                <p>Entradas</p>
+                <img src= {IncomeImg} alt="Entradas" />
+            </header>
+            <strong>R$1000,00</strong>
+        </div>
+        <div>
+            <header>
+                <p>Saidas</p>
+                <img src= {OutcomeImg} alt="Saídas" />
+            </header>
+            <strong> -R$5000,00</strong>
+        </div>
+        <div className="highlight-background">
+            <header>
+                <p>Total</p>
+                <img src= {TotalImg} alt="Total" />
+            </header>
+            <strong>R$500,00</strong>
+        </div>
+    </Container>
+    )
+}
