d7c4d1f57e16e67c9aa9f172435ef005135daece
Cássio Castro <cassiocastro222@gmail.com>
2022-03-17 13:53:20 -0300

    Correção de bugs no api.ts

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg' 
 import outcomeImg from '../../assets/outcome.svg' 
+import { api } from '../../services/api';
 
 import { 
     Container,
@@ -19,6 +20,23 @@ interface NewTransactionModalProps{
 export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps){
 
     const[ type, setType ]=useState('deposit');
+    const[ title, setTitle ]=useState('');
+    const[ value, setValue ]=useState(0);
+    const[ category, setCategory ]=useState('');
+
+
+    function handleCreateNewTransaction(event: FormEvent){
+        event.preventDefault();
+        const data = {
+            title,
+            value,
+            category,
+            type
+        };
+
+        api.post('/transactions', data)
+
+    }
 
 
     return(
@@ -35,16 +53,20 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
           </button>
 
 
-          <Container>
+          <Container onSubmit={handleCreateNewTransaction}>
             <h2>Cadastrar nova transação</h2>
 
             <input 
                 placeholder="Título" 
+                value={title}
+                onChange={event => setTitle(event.target.value)}
             />
 
             <input 
                 type="number"
                 placeholder='Valor' 
+                value={value}
+                onChange={event => setValue(Number(event.target.value))}
             />
 
             <TransactionTypeContainer>
@@ -70,7 +92,9 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
             </TransactionTypeContainer>
             
             <input 
-                placeholder='Categoria' 
+                placeholder='Categoria'
+                value={category} 
+                onChange={event => setCategory(event.target.value)}
             />
 
             <button
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', ()=>{
+      return [
+        {
+          lalala:123
+        }
+      ]
+    })
+
+    this.post('/transactions', (schema, request)=>{
+      const data = JSON.parse(request.requestBody)
+
+      return data;
+    })
+    
+  }
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
